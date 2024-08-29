58aa7beef3a8fe4db5bcad1352276231777af8b4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-29 15:04:18 +0600

    lraning string Data types

--- Milestone-4/Module-17/script.js
+++ Milestone-4/Module-17/script.js
@@ -8,11 +8,11 @@ console.log(studentName);
 const age = 18;
 console.log(age);
 console.log(typeof age);
-// //number/numeric
-// const country = 18;
-// console.log(age);
-// console.log(typeof age);
-// //number/numeric
-// const age = 18;
-// console.log(age);
-// console.log(typeof age);
+//number/numeric
+const country = Bangladesh;
+console.log(country);
+console.log(typeof age);
+//number/numeric
+const isRich = true;
+console.log(age);
+console.log(typeof age);

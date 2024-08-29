cfe05af661fb2e255053f09ee4e2fca56396948f
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-29 22:27:03 +0600

    learning variable nameing convention

--- Milestone-4/Module-17/script.js
+++ Milestone-4/Module-17/script.js
@@ -16,3 +16,10 @@ console.log(typeof country);
 const isRich = true;
 console.log(isRich);
 console.log(typeof isRich);
+
+//variable nameing conversions
+// Names can contain letters, digits, underscores, and dollar signs.
+// Names must begin with a letter.
+// Names can also begin with $ and _ (but we will not use it in this tutorial).
+// Names are case sensitive (y and Y are different variables).
+// Reserved words (like JavaScript keywords) cannot be used as names.

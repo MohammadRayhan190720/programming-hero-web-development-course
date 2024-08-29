92ef68c827f1be97002f17f79f4abc6be051b209
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-29 15:03:08 +0600

    learning Data types of Variables

--- Milestone-4/Module-17/script.js
+++ Milestone-4/Module-17/script.js
@@ -2,3 +2,17 @@ var classRoll = 15 ;
 console.log(classRoll);
 let studentName = "rayhan";
 console.log(studentName);
+
+//data types
+//number/numeric
+const age = 18;
+console.log(age);
+console.log(typeof age);
+// //number/numeric
+// const country = 18;
+// console.log(age);
+// console.log(typeof age);
+// //number/numeric
+// const age = 18;
+// console.log(age);
+// console.log(typeof age);

b59c264bb96cde8e89f17bd3806347a9394b30a1
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-30 14:44:59 +0600

    task-2 completed

--- Milestone-4/Module-18/script.js
+++ Milestone-4/Module-18/script.js
@@ -170,6 +170,23 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - Otherwise, you are obese.
 
 */
+let weight = 75;
+let height = 3.40;
+let BMI = weight / (height)
+if(BMI < 18.5){
+  console.log("you are underweight")
+}
+else if(BMI >= 18.5 && BMI <= 24.9){
+  console.log("you are normal");
+}
+  else if(BMI >= 25 && BMI <= 29.9){
+    console.log("you are overweight");
+  
+  }
+  else{
+    console.log("you are obese")
+  }
+
 
 //task-3
 

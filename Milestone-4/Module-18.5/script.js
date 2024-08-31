727819b2f114f553e2341edd2b2e2b358ae20109
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-31 08:47:16 +0600

    learning array

---
+++ Milestone-4/Module-18.5/script.js
@@ -0,0 +1,10 @@
+// Fundamental of Array
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(numbers.length)
+console.log(numbers[3])
+const fifth = numbers[5];
+console.log(fifth);
+
+numbers[2] = 555;
+console.log(numbers);

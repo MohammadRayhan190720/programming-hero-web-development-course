8c566ec0b08beddcf87725acc9f341872ba8c753
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-01 09:41:33 +0600

    Learning reallife conditional

--- Milestone-4/Module-18/script.js
+++ Milestone-4/Module-18/script.js
@@ -325,7 +325,22 @@ weather.addEventListener("change", setWeather);
 
 const setWeather = () => {
    if(weather === "sunny"){
-    
+    result2.textContent =
+      "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+   }
+   else if( weather === "Rainy"){
+    result2.textContent =
+      "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long.";
+   }
+   else if (weather === "snowing") {
+     result2.textContent =
+       "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
+   } else if (weather === "overcas") {
+     result2.textContent =
+       "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
+   }
+   else{
+    result2.textContent = "";
    }
 }
 

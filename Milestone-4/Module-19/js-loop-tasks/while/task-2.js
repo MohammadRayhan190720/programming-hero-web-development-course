c7315c37e2530325cde63ddcf6c75a358e8df854
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-02 16:11:50 +0600

    task-2 completed

--- Milestone-4/Module-19/js-loop-tasks/while/task-2.js
+++ Milestone-4/Module-19/js-loop-tasks/while/task-2.js
@@ -5,6 +5,13 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
+let oddNumber = 61;
+while ( oddNumber <=100){
+  if( oddNumber % 2 !== 0){
+    console.log("Odd Number: " + oddNumber)
+  }
+  oddNumber ++;
+}
 
 
 /***

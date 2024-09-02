98a6cbf836cca116dfdc448f3caf16e0a5ea0061
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-02 11:27:11 +0600

    Different ways to use loop

--- Milestone-4/Module-19/script.js
+++ Milestone-4/Module-19/script.js
@@ -1,18 +1,18 @@
 //for of loop
 
-const numbers = [12, 32, 14, 85, 25, 23];
+// const numbers = [12, 32, 14, 85, 25, 23];
 
-for (const num of numbers) {
-  console.log(num)
+// for (const num of numbers) {
+//   console.log(num)
 
   
-}
+// }
 
-const fruits = ["mango","banana","apple","jackfruits","orange"]
-for (const fruit of fruits) {
-  console.log(fruit);
-  console.log("I eat fruits: " + fruit);
-}
+// const fruits = ["mango","banana","apple","jackfruits","orange"]
+// for (const fruit of fruits) {
+//   console.log(fruit);
+//   console.log("I eat fruits: " + fruit);
+// }
 
 //while loop
 
@@ -23,97 +23,97 @@ for (const fruit of fruits) {
 //   console.log("Big number")
 // }
 
-let num = 0;
-while(num < 5){
-  console.log("Number: " + num)
-  // num ++;
-  // num = num + 1;
-     num += 1;
-}
+// let num = 0;
+// while(num < 5){
+//   console.log("Number: " + num)
+//   // num ++;
+//   // num = num + 1;
+//      num += 1;
+// }
 
 //while loop problem solveing
 
 //print 1-10;
 
-let num1 = 1;
-while(num1 <= 10){
-  console.log(num1);
-  num1++;
-}
+// let num1 = 1;
+// while(num1 <= 10){
+//   console.log(num1);
+//   num1++;
+// }
 
 //print 1-100
 
-let num2 = 1;
-while (num1 <= 100) {
-  console.log(num1);
-  num1++;
-}
+// let num2 = 1;
+// while (num1 <= 100) {
+//   console.log(num1);
+//   num1++;
+// }
 
 //sum of 1-10
 
-let num3 = 1 ;
-let sum = 0 ;
+// let num3 = 1 ;
+// let sum = 0 ;
 
-while( num3 <= 10){
-  sum = sum + num3;
-  console.log(sum)
-  num3++;
-}
+// while( num3 <= 10){
+//   sum = sum + num3;
+//   console.log(sum)
+//   num3++;
+// }
 
 //print even number
 
-let num4 = 1 ;
-while( num4 <=10){
-  if(num4 % 2 ===  0){
-    console.log("Even numbr is " + num4)
-  }
- num4++;
+// let num4 = 1 ;
+// while( num4 <=10){
+//   if(num4 % 2 ===  0){
+//     console.log("Even numbr is " + num4)
+//   }
+//  num4++;
 
-}
+// }
 
 // for loop
 
-for ( let i = 0; i<=10 ; i++){
-  console.log(i)
-}
+// for ( let i = 0; i<=10 ; i++){
+//   console.log(i)
+// }
 
 //even number print
 
-for (let i = 0; i<= 20 ; i+=2){
-  console.log(i)
-}
+// for (let i = 0; i<= 20 ; i+=2){
+//   console.log(i)
+// }
 //odd number print
 
-for (let i = 1; i<= 20 ; i+=2){
-  console.log(i)
-}
+// for (let i = 1; i<= 20 ; i+=2){
+//   console.log(i)
+// }
 
 //for loop problem solveing
 
 //sum of 1-20 numbers
 
-let sum1 = 0;
+// let sum1 = 0;
 
-for( i = 1; i<=20 ; i++){
-  console.log(i);
-  sum1 = sum1 + i ;
+// for( i = 1; i<=20 ; i++){
+//   console.log(i);
+//   sum1 = sum1 + i ;
 
-}
-console.log("sum of 1-20 is :" + sum1)
+// }
+// console.log("sum of 1-20 is :" + sum1)
 
 //decrement
 
-for ( i = 10; i >=0 ; i--){
-  console.log(i)
-}
+// for ( i = 10; i >=0 ; i--){
+//   console.log(i)
+// }
 
 //while loop decrement
 
-let num5 = 10 ;
-while ( num5 >= 0){
-  console.log(num5);
-  num5--;
-}
+// let num5 = 10 ;
+// while ( num5 >= 0){
+//   console.log(num5);
+//   num5--;
+// }
 
 //different ways to use loop
 // odd number print 1-20
@@ -126,34 +126,34 @@ while ( num5 >= 0){
 // }
 
 //2
-for( i = 0 ; i <= 20 ; i++){
-  if ( i % 2 !== 0){
-    console.log(i);
-  }
-}
+// for( i = 0 ; i <= 20 ; i++){
+//   if ( i % 2 !== 0){
+//     console.log(i);
+//   }
+// }
 
 //3
 
-for (i = 1; i <= 20; i+=2) {
-  console.log(i);
+// for (i = 1; i <= 20; i+=2) {
+//   console.log(i);
   
-}
+// }
 
 //list of number between 1-30 divided by 5
 
-for ( i = 1 ; i <=30 ; i++){
-  if( i % 5 === 0){
-    console.log(i)
-  }
-}
+// for ( i = 1 ; i <=30 ; i++){
+//   if( i % 5 === 0){
+//     console.log(i)
+//   }
+// }
 
 //list of number between 1-30 divided by 3
 
-for ( i = 1 ; i <= 30 ; i++){
-  if( i % 3 === 0){
-    console.log(i)
-  }
-}
+// for ( i = 1 ; i <= 30 ; i++){
+//   if( i % 3 === 0){
+//     console.log(i)
+//   }
+// }
 
 //list of number between 1-30 divided by 3 and 5
 for ( i = 1 ; i <= 30 ; i++){
@@ -163,8 +163,23 @@ for ( i = 1 ; i <= 30 ; i++){
 }
 
 
-
-
-
 //list of number between 1-30 divided by 3 or 5
+// for (i = 1; i <= 30; i++) {
+//   if (i % 3 === 0 || i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//list of number between 1-30 divided by 3 and sum of these number
+let sum = 0 ;
+for (i = 1; i <= 30; i++) {
+  if (i % 3 === 0 ) {
+    console.log(i);
+    sum = sum + i ;
+    console.log("total step setp by sum " + sum)
+  }
+}
+
+console.log("Total sum of number between 1 -30 : " + sum)
+
 

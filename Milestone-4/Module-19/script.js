8b7e885b855ec16203b4b76403fc55b7bd446ffa
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-02 10:00:58 +0600

    learning for loop

--- Milestone-4/Module-19/script.js
+++ Milestone-4/Module-19/script.js
@@ -87,3 +87,17 @@ for (let i = 0; i<= 20 ; i+=2){
 for (let i = 1; i<= 20 ; i+=2){
   console.log(i)
 }
+
+//for loop problem solveing
+
+//sum of 1-20 numbers
+
+let sum1 = 0;
+
+for( i = 1; i<=20 ; i++){
+  console.log(i);
+  sum1 = sum1 + i ;
+
+}
+console.log("sum of 1-20 is :" + sum1)
+

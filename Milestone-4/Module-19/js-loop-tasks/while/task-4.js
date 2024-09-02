39dd5b67df18bbe10b68ddcc1cf3e039438b7cd2
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-02 16:36:42 +0600

    task-4 completed

---
+++ Milestone-4/Module-19/js-loop-tasks/while/task-4.js
@@ -0,0 +1,13 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+let number = 1;
+let multiplication = 5;
+while ( number <= 10){
+ const  multiplicationTable = multiplication * number ;
+  number++;
+  console.log(multiplicationTable);
+}

18de8a1b6d08d14e2d00617f6412a68c7b3ce02e
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-02 08:48:57 +0600

    learning while loop

--- Milestone-4/Module-19/script.js
+++ Milestone-4/Module-19/script.js
@@ -13,3 +13,20 @@ for (const fruit of fruits) {
   console.log(fruit);
   console.log("I eat fruits: " + fruit);
 }
+
+//while loop
+
+// const num = 7;
+// if(num < 10){
+//   console.log("Small number")
+// } else{
+//   console.log("Big number")
+// }
+
+let num = 0;
+while(num < 5){
+  console.log("Number: " + num)
+  // num ++;
+  // num = num + 1;
+     num += 1;
+}

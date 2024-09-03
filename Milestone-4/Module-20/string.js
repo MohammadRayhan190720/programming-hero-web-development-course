a3ca8080916974c2150dc867046f9ff518946447
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-03 07:39:38 +0600

    learning string

---
+++ Milestone-4/Module-20/string.js
@@ -0,0 +1,22 @@
+// string declaration
+
+const country = 'Bangladesh';
+const nationalGame = "Criclet";
+const primeminister = `Dr. Mohammad Younus`;
+const money = new String('onlu money is real');
+
+//array
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(numbers[3])
+numbers[3] = 100;
+console.log(numbers);
+console.log(numbers.length);
+
+//string
+
+const student = "Rayhan";
+console.log(student.length);
+console.log(student[3]);
+
+//string is immutable => not changeable
+// array is mutable => changable

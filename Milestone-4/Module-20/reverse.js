2fb43e4e5b3402d19d2326993f80acbbde75f771
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-03 08:59:19 +0600

    learning reverse a string

---
+++ Milestone-4/Module-20/reverse.js
@@ -0,0 +1,11 @@
+const sentence = 'I am learning web development';
+
+let reverse = '';
+for ( let letter of sentence){
+  // console.log(letter);
+  reverse = letter + reverse;
+}
+  console.log(reverse);
+
+
+

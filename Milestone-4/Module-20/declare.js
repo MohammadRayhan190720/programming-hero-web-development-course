aa9b51a3bcb34510a8c7bf0943dca785207a8b78
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-03 11:42:17 +0600

    learning object

---
+++ Milestone-4/Module-20/declare.js
@@ -0,0 +1,14 @@
+const studentInfo = {name : 'Rayhan',className : 12, id: 101}
+
+const workerInfo = {
+  name : "Kabul",
+  age : 35,
+  location : "rangpur"
+}
+
+const fermerInfo = new Object({
+  name : "Korim",
+  time : 5.00,
+  condition : "ill"
+});
+console.log(fermerInfo);

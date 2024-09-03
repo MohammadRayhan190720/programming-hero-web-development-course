69ca259374e0699291ef0dbd9689cb445808d6c1
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-03 09:06:05 +0600

    learning reverse a string

--- Milestone-4/Module-20/reverse.js
+++ Milestone-4/Module-20/reverse.js
@@ -8,4 +8,21 @@ for ( let letter of sentence){
   console.log(reverse);
 
 
+//for loop
 
+let rev = '';
+
+for ( let i = 0; i < sentence.length; i++){
+  // console.log(i);
+  // console.log(sentence[i]);
+  let letters = sentence[i];
+  rev = letters + rev;
+}
+console.log(rev);
+
+
+//split()
+
+let reversed = sentence.split('').reverse().join('');
+
+console.log(reversed);

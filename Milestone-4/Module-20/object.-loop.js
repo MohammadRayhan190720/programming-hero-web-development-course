a109b72d60bc8cc3e5d056e96963fa0cd0774e87
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-03 11:34:07 +0600

    learning object

---
+++ Milestone-4/Module-20/object.-loop.js
@@ -0,0 +1,13 @@
+const mobile = {
+  brand : 'realme',
+  prize : 17000,
+  battay : '5000MP',
+  charger : 'orginal',
+  display : "6 inces",
+  software : ['opera-mini','crome-browser', 'firefox','facebook','youtube']
+}
+
+for ( const key in mobile){
+  // console.log(key);
+  console.log(mobile[key]);
+}

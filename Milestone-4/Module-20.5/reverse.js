a2d262ec7fe476355aa9b07dbf8f4b2977db9be4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-04 09:29:11 +0600

    Learning array reverse

--- Milestone-4/Module-20.5/reverse.js
+++ Milestone-4/Module-20.5/reverse.js
@@ -2,9 +2,22 @@ const numbers = [2,3,4,8,9,12];
 // numbers.reverse()
 // console.log(numbers);
 
-let revNumber = [];
+// let revNumber = [];
 
-for (const number of numbers) {
-  revNumber.unshift(number);
+// for (const number of numbers) {
+//   revNumber.unshift(number);
+// }
+  // console.log(revNumber);
+
+
+//for loop
+
+let reversed = [];
+
+for ( let i = numbers.length - 1; i >= 0; i-- ) {
+  // console.log(numbers[i]);
+const num = numbers[i];
+// console.log(num);
+reversed.push(num);
 }
-  console.log(revNumber);
+console.log(reversed);

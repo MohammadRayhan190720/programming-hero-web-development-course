01f563728b919c918a2e36750f6e29e9ab954fb4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-04 19:32:02 +0600

    Task-2 object completed

--- Milestone-4/Module-20/object-task.js
+++ Milestone-4/Module-20/object-task.js
@@ -24,41 +24,48 @@ console.log(car);
 // Task-3
 // Display the physics marks as output.
 
-// const student = {
-//     name: "Hamim Sakep",
-//     id: 5421,
-//     physics: {
-//         subject: "HSC Physics",
-//         author: "Shahjahan Tapan",
-//         marks: 30
-//     }
-// };
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+const physicsMarks = student.physics.marks;
+console.log(physicsMarks);
 
 // Task-4
 // Count the number of properties.
 
 // Input:
 
-// let student = {
-//     name: 'Ariana Grande',
-//     age: 21,
-//     city: 'Gaibandha',
-//     isStudent: true
-// };
-// Output: 4;
+let students = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+let keys = Object.keys(students)
+console.log(keys.length);
+
+
 
 // Task-5 (Hard)
 // Loop through an object and print the key-value pairs with their types
 
 // Input:
 
-// let myObject = {
+let myObject = {
 
-// name: 'John Doe',
-// age: 25,
-// city: 'Example City',
-// isStudent: true
-// };
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
 // Output:
 
 

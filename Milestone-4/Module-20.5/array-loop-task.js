ddfefac03bf4ab48694c9f5310adc4d0d2361b93
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-04 20:21:47 +0600

    task-1 completed array

--- Milestone-4/Module-20.5/array-loop-task.js
+++ Milestone-4/Module-20.5/array-loop-task.js
@@ -29,7 +29,16 @@ for ( let i = colors.length - 1; i >= 0; i-- ) {
 // Task 2
 // Write a JavaScript code to get the even numbers from an array using any looping technique.
 
-// Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
+ const numbers = [12, 98, 5, 41, 23, 78, 46];
+ const evenNumbers =[];
+
+for (let i = 0; i < numbers.length; i++) {
+  if ( numbers[i] % 2 === 0){
+    evenNumbers.push(numbers[i])
+  }
+} 
+
+console.log(evenNumbers)
 
 // Output:
 

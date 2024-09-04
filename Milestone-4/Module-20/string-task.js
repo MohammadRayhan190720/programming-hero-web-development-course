6ae0a0a6bff798b1c8f83be21e46720461e415b9
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-04 12:08:34 +0600

    Solveing string task-3

--- Milestone-4/Module-20/string-task.js
+++ Milestone-4/Module-20/string-task.js
@@ -44,6 +44,27 @@ console.log(countLetterA1(sentence1));
 // Task-3:
 // Check whether a string contains all the vowels a, e, i, o, u
 
+const sentence2 = "My Name is Apple And I love to eat Apple and a Ant is Very"
+
+function checkVowels (str){
+  str = str.toLowerCase();
+
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  for (let vowel of vowels) {
+    if (!str.includes(vowel)){
+      return false;
+    }
+    
+  }
+  return true;
+
+ }
+ console.log(checkVowels(sentence2));
+
+
+
+
 // Task-4:
 // If a given string has either x, replace x by y. if the given string has X, replace it by Y.
 // Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.

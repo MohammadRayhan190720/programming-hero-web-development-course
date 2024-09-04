481e71faafc749c39a03c724c972a10a8b1d23ce
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-04 09:46:42 +0600

    Learning sort

---
+++ Milestone-4/Module-20.5/sort.js
@@ -0,0 +1,8 @@
+const friends = ['sakib','tamim','Taskin','Rana'];
+console.log(friends.sort())
+
+const numbers = [1,4,52,62,42,8,7,4];
+numbers.sort(function (a,b) {
+  return a-b
+});
+console.log(numbers);

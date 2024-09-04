4c88352348856da1bceb4264d9611838bd345180
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-04 08:00:29 +0600

    Learning array loop

--- Milestone-4/Module-20.5/for-loop.js
+++ Milestone-4/Module-20.5/for-loop.js
@@ -1,5 +1,14 @@
 const friends = [ 'Elon' , 'Mask','Mark' , 'Bill'];
 
-for (var i = 0; i < friends.length; i++){
+// for (var i = 0; i < friends.length; i++){
+  // console.log(friends[i]);
+// }
+
+
+let i = 0;
+
+while (i < friends.length){
   console.log(friends[i]);
+
+  i++;
 }

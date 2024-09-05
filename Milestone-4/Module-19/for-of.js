6944e8d9f40d4bf4f79af1386972f7b4c04efdb9
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-05 10:29:09 +0600

    Rivision Module-19

---
+++ Milestone-4/Module-19/for-of.js
@@ -0,0 +1,8 @@
+//for of loop
+
+const numbers = [12, 32, 14, 85, 25, 23];
+
+for (const num of numbers) {
+  console.log(num);
+  
+}
--- Milestone-4/Module-19/script.js
+++ Milestone-4/Module-19/script.js
@@ -1,12 +1,4 @@
-//for of loop
 
-// const numbers = [12, 32, 14, 85, 25, 23];
-
-// for (const num of numbers) {
-//   console.log(num)
-
-  
-// }
 
 // const fruits = ["mango","banana","apple","jackfruits","orange"]
 // for (const fruit of fruits) {

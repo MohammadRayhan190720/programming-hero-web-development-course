bbea001283c31d00f25ffd3c621907af36350b0d
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-06 10:50:13 +0600

    Learning function return

---
+++ Milestone-4/Module-21/function-return.js
@@ -0,0 +1,15 @@
+function tentimes(number){
+  const result = number * 10;
+  return result ;
+
+}
+function cutHalf (number){
+  const half = number / 2 ;
+  return half ;
+
+}
+
+tentimes (10);
+// console.log(tentimes(10));
+const finalResult = tentimes(10);
+console.log(finalResult);

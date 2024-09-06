836add24055706a68e0bf3d09cb78cbda726a53f
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-06 08:16:44 +0600

    Learning basic function and Syntex

---
+++ Milestone-4/Module-21/function.js
@@ -0,0 +1,26 @@
+
+// what is function is javascript?
+// A Block Of Code
+// A set of statements that performs a tasks when it is called
+// maintains relationship with input and Output
+
+
+//function inproved code
+// 1. reuseability
+// 2. readability
+
+// different between loop and Function
+
+//loop- loop akbar suru hole shate shate aktana barbar colbe
+
+//function- jekno somoy jekno kajer por function call kore jabe
+
+
+
+//declare a function
+
+function fanOff(){
+  console.log("Stand up and going towords Fan")
+  console.log("Off the fan Switch");
+}
+fanOff();

d9b0689d20341bfa51367209900c72b42bda2d19
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-06 11:19:25 +0600

    Learning function return more datails

--- Milestone-4/Module-21/recap-return.js
+++ Milestone-4/Module-21/recap-return.js
@@ -14,3 +14,14 @@ function add1 (price1,price2){
 }
 const totalBill1 = add1(5,80);
 console.log(totalBill1);
+
+
+function doMath (num1,num2){
+  const sum = num1 + num2;
+  const diff = num1 - num2;
+  const multiply = sum * diff;
+  const result = multiply / 2 ;
+  return result ;
+}
+const finalResult = doMath(50,10);
+console.log(finalResult);

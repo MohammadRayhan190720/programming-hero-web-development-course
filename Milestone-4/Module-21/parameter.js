3c106ccd148a9964b80714aed8afc91a0583188d
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-06 09:33:05 +0600

    Learning parameter in function

--- Milestone-4/Module-21/parameter.js
+++ Milestone-4/Module-21/parameter.js
@@ -6,3 +6,15 @@ function squareNumber (number){
 }
 
 squareNumber(8);
+
+//sum of 2 numbers
+
+function addNumbers (num1, num2){
+  const sum = num1 + num2;
+  console.log(`sum of ${num1} and ${num2} is ${sum}`);
+
+}
+addNumbers(20,30);
+addNumbers(50,30);
+addNumbers(40,30);
+addNumbers(10,30);

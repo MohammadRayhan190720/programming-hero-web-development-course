78beb9f9fedb94a2af4e247645b6d94c94776f43
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-06 15:06:50 +0600

    learing Different Type of String

--- Milestone-4/Module-21/diff-params.js
+++ Milestone-4/Module-21/diff-params.js
@@ -14,3 +14,21 @@ function evenCheckString(str){
 
 evenCheckString("Bangladesh")
 console.log(evenCheckString("Bangladesh"))
+
+
+//program-2
+
+function returnDoubleTripple( number,ifDouble){
+  if ( ifDouble === true){
+    const result = number * 2;
+    return result;
+  }
+  else{
+    const result = number * 3;
+    return result;
+  }
+
+}
+
+console.log(returnDoubleTripple(10,true));
+console.log(returnDoubleTripple(10,false));

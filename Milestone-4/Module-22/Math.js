67bee0919c3f5295947336862110215269494c88
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-07 12:48:41 +0600

    learning Math Object in js

---
+++ Milestone-4/Module-22/Math.js
@@ -0,0 +1,13 @@
+const numbers = Math.min(20,10,1,5,60,2);
+console.log(numbers);
+const numbers2 = Math.max (20,10,1,5,60,2);
+console.log(numbers2);
+
+console.log(Math.PI);
+
+console.log(Math.abs(-95));
+console.log(Math.round(3.50));
+console.log(Math.floor(3.50));
+console.log(Math.ceil(3.1))
+console.log(Math.random() * 10)
+console.log(Math.floor(Math.random() * 10))

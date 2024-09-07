79a34967b3755703c56b415cf0ac6c0199545cd0
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-07 11:02:04 +0600

    leap year problem solveing

--- Milestone-4/Module-22/leep-year.js
+++ Milestone-4/Module-22/leep-year.js
@@ -1,11 +1,32 @@
 //kno year leep year kina ber kro
 
-const isLeapYear = (year) => {
-  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
-    console.log(`${year} is a leap year`);
-  } else {
-    console.log(`${year} is not a leap year`);
-  }
-};
+//leap year => if the year not divisible by 100 and Divisible by 4 
+//leap year => if the year divisible by 100 and divisible by 400
 
-isLeapYear(2020);
+// const isLeapYear = (year) => {
+//   if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+//     console.log(`${year} is a leap year`);
+//   } else {
+//     console.log(`${year} is not a leap year`);
+//   }
+// };
+
+// isLeapYear(2020);
+
+function isLeapYear(year){
+  if ( year % 100 !== 0  && year % 4 === 0){
+      return true;
+
+  } 
+  else if ( year % 400 === 0 && year % 100 === 0){
+    return true
+  }
+  else{
+    return false;
+  }
+
+
+}
+const isLipi = isLeapYear(2022);
+console.log(isLipi);
+

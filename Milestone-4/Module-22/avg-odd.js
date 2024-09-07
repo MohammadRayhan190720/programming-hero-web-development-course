99614205f69fb15560b8430d3f39da471cd14772
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-07 11:37:54 +0600

    array even number average

--- Milestone-4/Module-22/avg-odd.js
+++ Milestone-4/Module-22/avg-odd.js
@@ -22,3 +22,29 @@ const oddNumbers = [];
  const numbers = [5,10,15,16,18,25];
  const average = oddAverage(numbers);
  console.log(`odd average: ${average}`);
+
+
+ //even number average
+ let evenNumbers = [];
+
+ function evenAverage(numbers){
+  for (const number of numbers){
+    if ( number % 2 === 0){
+      evenNumbers.push(number);
+    }
+
+  
+  }
+  let sum = 0;
+
+  let arrayLen = evenNumbers.length;
+  for (const evenNumber of evenNumbers){
+    sum += evenNumber;
+  }
+  return sum / arrayLen;
+
+
+ }
+ const numbers2 = [ 20,40,15,19,20];
+ const evenAvg = evenAverage(numbers2);
+ console.log(`even average: ${evenAvg}`);

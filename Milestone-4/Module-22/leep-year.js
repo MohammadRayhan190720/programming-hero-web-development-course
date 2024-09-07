6030e273fbf2da41a9893c859865dcc15222e8fa
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-07 10:47:40 +0600

    leap year problem solveing

---
+++ Milestone-4/Module-22/leep-year.js
@@ -0,0 +1,11 @@
+//kno year leep year kina ber kro
+
+const isLeapYear = (year) => {
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    console.log(`${year} is a leap year`);
+  } else {
+    console.log(`${year} is not a leap year`);
+  }
+};
+
+isLeapYear(2020);

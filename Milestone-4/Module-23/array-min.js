c527823b98aa1056a6ac96551bdd78e910d7322a
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-08 12:16:00 +0600

    learning array min value

--- Milestone-4/Module-23/array-min.js
+++ Milestone-4/Module-23/array-min.js
@@ -1,22 +1,22 @@
 //find the cheapest phone form an Array
 
 
-// const phonePrizes = [20000, 15000, 25000, 30000, 40000, 35000];
+const phonePrizes = [20000, 15000, 25000, 30000, 40000, 35000];
 
-// function cheapestPhone (numbers){
-//   let cheapPrize = numbers[0];
-//   for (let number of numbers) {
-//     if ( number < cheapPrize ) {
-//          cheapPrize = number;
+function cheapestPhone (numbers){
+  let cheapPrize = numbers[0];
+  for (let number of numbers) {
+    if ( number < cheapPrize ) {
+         cheapPrize = number;
 
-//     }
+    }
   
-//   }
-//   return cheapPrize;
+  }
+  return cheapPrize;
 
-// }
-// const cheapPhone = cheapestPhone(phonePrizes);
-// console.log("cheap phone prizes: " + cheapPhone);
+}
+const cheapPhone = cheapestPhone(phonePrizes);
+console.log("cheap phone prizes: " + cheapPhone);
 
 
 //find the cheapest phone form an Array of Phone Objects

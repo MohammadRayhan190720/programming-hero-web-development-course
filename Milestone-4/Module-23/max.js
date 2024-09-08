be182ab9fceb9f8f30ca4e1ac142cf27bee96638
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-08 09:36:08 +0600

    Learning number of max

--- Milestone-4/Module-23/max.js
+++ Milestone-4/Module-23/max.js
@@ -25,3 +25,35 @@ const ultimateMax = biggerMarks(max1,max2);
 console.log(`Height number: ${ultimateMax}`);
 console.log(`Height number: ${heightestMarks}`);
 
+
+//maxof three numbers
+
+const kim = 80;
+const jim = 90;
+const putin = 70;
+
+if (kim > jim && kim > putin){
+  console.log("max number is " + kim);
+  
+}
+else if( jim > kim && jim > putin){
+  console.log("max number is " + jim);
+}else{
+  console.log("max number is " + putin);
+}
+
+function maxNumber (num1,num2,num3){
+  if ( num1 > num2 && num1 > num3){
+    return num1;
+  }
+  else if( num2 > num3 && num2 > num1){
+    return num2;
+  }
+  else {
+    return num3;
+  }
+
+}
+const maxOfThree = maxNumber(kim,jim,putin);
+console.log(maxOfThree);
+

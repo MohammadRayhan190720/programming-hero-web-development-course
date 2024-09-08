b09c22f595cbd173abbd0a6f854192d067db9708
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-08 10:03:59 +0600

    min number form an array

--- Milestone-4/Module-23/array-max.js
+++ Milestone-4/Module-23/array-max.js
@@ -12,3 +12,19 @@ function maxHeight (numbers){
 const height = [65,70,56,42,62,65];
 const maxHeightNumber = maxHeight(height);
 console.log(maxHeightNumber);
+
+
+//min number form an Array
+
+function minHeight(numbers){
+  let min = numbers[0];
+  for (const number of numbers) {
+    if ( number < min){
+      min = number;
+    }
+  }
+  return min;
+
+}
+const minHeightNumber = minHeight(height);
+console.log(minHeightNumber);

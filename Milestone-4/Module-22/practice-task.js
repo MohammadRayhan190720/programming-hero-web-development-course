bff6d925aa8910ddf74ad7abc5e52698f8079774
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-08 08:14:11 +0600

    Interview episode-6 added

--- Milestone-4/Module-22/practice-task.js
+++ Milestone-4/Module-22/practice-task.js
@@ -16,7 +16,27 @@ console.log(result);
 // Task-2:
 // You are given an array of numbers. Count how many times the a number is repeated in the array.
 
-// sample-input: numbers = [5,6,11,12,98, 5]
+// sample-input: 
+const numbers = [5,6,11,12,98, 5];
+
+function countRepetedNumbers(numbers){
+  let count = 0;
+  let noRepeted = [];
+  let repeted = [];
+  for (const number of numbers) {
+     if ( noRepeted.includes(number) === false){
+         noRepeted.push(number);
+     }
+      
+  }
+  return noRepeted;
+
+}
+const count = countRepetedNumbers(numbers);
+console.log(count);
+
+
+
 
 // find: 5
 
@@ -34,6 +54,18 @@ console.log(result);
 // Task-3:
 // Write a function to count the number of vowels in a string.
 
+function vowelCount (string){
+  for (const letter of string) {
+    console.log(letter);
+    
+  }
+
+}
+const sentence = "Bangladesh";
+const countV = vowelCount(sentence);
+console.log(countV);
+
+
 
 // Task-4:
 // Write a function to find the longest word in a given string.
@@ -44,3 +76,6 @@ console.log(result);
 
 // Task-5:
 // Generate a random number between 10 to 20.
+
+const randomNumber = Math.floor(Math.random() * 11 + 10);
+console.log(randomNumber);

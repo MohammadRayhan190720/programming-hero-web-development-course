8f153bd5e77ed72c768d41a6294d2d06407f71c9
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-09 09:36:06 +0600

    solving problem

--- Milestone-4/Module-23/practice-task.js
+++ Milestone-4/Module-23/practice-task.js
@@ -21,7 +21,14 @@ console.log(lowestNumber);
 
 // Task -2:
 // Find the friend with the smallest name.
-// const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+function smallestName(name){
+
+}
+
+
+const students = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+const smallName = smallestName(students);
+console.log(smallName);
 
 
 

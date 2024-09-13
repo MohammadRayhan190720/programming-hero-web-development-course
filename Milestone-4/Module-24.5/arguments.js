4928c70a6ef87fb86efbb14f7aa63398c6fc2bf5
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-13 19:51:14 +0600

    learning arguments

--- Milestone-4/Module-24.5/arguments.js
+++ Milestone-4/Module-24.5/arguments.js
@@ -1,6 +1,7 @@
 function add (num1,num2){
   console.log(num1 , num2);
   console.log(arguments)  // array like objects
+  console.log(arguments[4])  // array like objects
 
 }
 add(12,13,15,20,17,18,19,16);

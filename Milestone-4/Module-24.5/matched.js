d7be79623eacc5b5b0d34399e49901a79b75c500
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-13 20:19:53 +0600

    learning matched js

--- Milestone-4/Module-24.5/matched.js
+++ Milestone-4/Module-24.5/matched.js
@@ -4,6 +4,24 @@ const numbers = [ 25, 30, 10 ,12 ,15];
 //   console.log(number);
 // }
 
-for (const number of numbers) {
-  console.log(number);
-}
+// for (const number of numbers) {
+//   console.log(number);
+// }
+
+
+
+const products = [
+  { id : "1" , name : "Tecno phone" , price : 15000},
+  { id : "2" , name : "Nokia phone" , price : 15000},
+  { id : "3" , name : "Sungsung phone" , price : 15000},
+  { id : "4" , name : "Lexo phone" , price : 15000},
+  { id : "5" , name : "Realme phone" , price : 15000},
+  { id : "6" , name : "HP Laptop" , price : 15000},
+  { id : "7" , name : "Lenovo laptop" , price : 15000},
+  { id : "8" , name : "Asus Latop" , price : 15000},
+  { id : "9" , name : "Walton Laptop" , price : 15000},
+  { id : "10" , name : "Apple Laptop" , price : 15000},
+  { id : "11" , name : "One Plus Phone" , price : 15000},
+
+
+]

cb403c886f934b64c11d8df569202deac2f0e5be
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-13 19:27:52 +0600

    Learning object datails

--- Milestone-4/Module-24.5/object.js
+++ Milestone-4/Module-24.5/object.js
@@ -2,5 +2,11 @@ const students = {
   name : "Sakib",
   id : " 101",
   roll : " 102",
-  address : []
+  address : [
+    {
+      recent_address : "USA",
+      permanet_address : "Bangladesh",
+
+    }
+  ]
 }

af7f6bce95e4d9225b8f518642146178f58ea343
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-14 21:57:47 +0600

    learning DOM

--- Milestone-5/Module-25/script.js
+++ Milestone-5/Module-25/script.js
@@ -1,4 +1,10 @@
 console.log("Leatning Javascript DOM");
 
 const liCollection = document.getElementsByTagName('li');
-console.log(liCollection);
+// console.log(liCollection);
+
+for (const li of liCollection) {
+  // console.log(li);   // array like object
+  console.log(li.innerText);
+  
+}

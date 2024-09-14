9350a460909c65747f832c05b9f359c9f1c8e116
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-14 22:21:54 +0600

    learning DOM

--- Milestone-5/Module-25/script.js
+++ Milestone-5/Module-25/script.js
@@ -13,8 +13,14 @@ for (const li of liCollection) {
 //id secector
 
 const section2 = document.getElementById('section-2');
+console.log(section2);
+// section2.innerText = "changed Heading By js";
 // console.log(section2.innerText);
-section2.innerText = "changed Heading By js";
-console.log(section2.innerText);
 
 // class selectors
+const skills = document.getElementsByClassName('must-learn');
+// console.log(skills);
+for (const skill of skills) {
+  console.log(skill);
+  
+}

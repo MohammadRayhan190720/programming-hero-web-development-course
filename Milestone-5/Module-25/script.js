cf9ea538d5d27326afc609b15b63c0d1addd2f38
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-14 23:41:46 +0600

    learning DOM

--- Milestone-5/Module-25/script.js
+++ Milestone-5/Module-25/script.js
@@ -31,4 +31,21 @@ for (const skill of skills) {
 
 //option-4 document.querySelector()
 const someLi = document.querySelector('li');
-console.log(someLi);
+// console.log(someLi);
+
+
+//option-5 document.querySelectorAll()
+const someList = document.querySelectorAll('.must-learn');
+console.log(someList);
+
+//different between HTML collection vs Node List
+
+// 1. HTML Collection (এইচটিএমএল কালেকশন):
+// এটি হলো এমন একটি তালিকা (list) যা HTML এর নির্দিষ্ট এলিমেন্টগুলোকে সংগ্রহ করে।
+// এই তালিকাটি লাইভ (Live) হয়। এর মানে হল, যদি পেজে কোন এলিমেন্ট যোগ হয় বা মুছে ফেলা হয়, তাহলে HTML Collection এর তালিকাটাও স্বয়ংক্রিয়ভাবে আপডেট হয়ে যায়।
+// এটি সাধারণত কিছু নির্দিষ্ট মেথড যেমন getElementsByClassName() বা getElementsByTagName() থেকে পাওয়া যায়।
+
+//2. NodeList (নোড লিস্ট):
+// এটি নোড বা HTML এলিমেন্টগুলোর তালিকা, যা সাধারণত querySelectorAll() মেথড থেকে পাওয়া যায়।
+// NodeList লাইভ নাও হতে পারে। অর্থাৎ, যদি আপনি নতুন এলিমেন্ট যোগ করেন, তাহলে সেই পরিবর্তন NodeList-এ স্বয়ংক্রিয়ভাবে আপডেট নাও হতে পারে।
+// এটি দেখতে অনেকটা অ্যারের মতো হলেও সব সময় অ্যারের মত কাজ করে না, তবে এর উপর forEach() লুপ চালানো যায়।

d42b0738bdcf44104c60e951cc471c3070eb95f8
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-15 06:25:08 +0600

    learning DOM

---
+++ Milestone-5/Module-25/styleinJs.js
@@ -0,0 +1,11 @@
+const allSection = document.querySelectorAll('section');
+// console.log(allSection);
+for (const section of allSection) {
+  // console.log(section);
+  section.style.border = '1px solid gray';
+  section.style.paddingBottom = "20px"
+  section.style.marginBottom = "20px"
+  section.style.borderRadius = "10px";
+  section.style.backgroundColor = "gray";
+  
+}

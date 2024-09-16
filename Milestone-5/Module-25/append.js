70c2c984e3157d83818414b011eaaa9c1e50f2c6
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-16 08:55:56 +0600

    Learning DOM

--- Milestone-5/Module-25/append.js
+++ Milestone-5/Module-25/append.js
@@ -8,23 +8,28 @@ const section = document.createElement('section');
 const h3 = document.createElement('h3');
 h3.innerText = "Learning Javascript-4";
 section.appendChild(h3);
+
 const ul = document.createElement('ul');
 const li1 = document.createElement('li');
-li5.innerText = "skill-1";
+li1.innerText = "skill-1";
 ul.appendChild(li1);
 
 const li2 = document.createElement('li');
-li5.innerText = "skill-2";
+li2.innerText = "skill-2";
 ul.appendChild(li2);
 
 const li3 = document.createElement('li');
-li5.innerText = "skill-3";
+li3.innerText = "skill-3";
 ul.appendChild(li3);
 
 const li4 = document.createElement('li');
-li5.innerText = "skill-4";
+li4.innerText = "skill-4";
 ul.appendChild(li4);
 
 const li5 = document.createElement('li');
 li5.innerText = "skill-5";
 ul.appendChild(li5);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);

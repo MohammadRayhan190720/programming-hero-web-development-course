393c6b93bb7b8ddc4fca4ec611c057a7469b0006
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-16 15:42:06 +0600

    Learning event

--- Milestone-5/Module-26/event-sumary.js
+++ Milestone-5/Module-26/event-sumary.js
@@ -2,3 +2,22 @@ function clickHandler(){
   const changeEvent = document.getElementById('text');
   changeEvent.innerText = 'This is Dynamic change Text';
 }
+
+// document.getElementById("clicked-btn").addEventListener("click", function () {
+//   console.log("clicked");
+// });
+
+document.getElementById('clicked-btn').addEventListener('click',function(){
+  const changeText = document.getElementById('text');
+  changeText.innerText = 'This is Dynamic change Text by event Listener';
+})
+
+
+document.getElementById('update-btn').addEventListener('click', function(){
+  const inputText = document.getElementById('input-text');
+  const changText = inputText.value;
+   
+  const p = document.getElementById('text-change');
+  p.innerText = changText;
+
+})

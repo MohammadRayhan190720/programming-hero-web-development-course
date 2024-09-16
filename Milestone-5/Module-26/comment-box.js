c72c6c85379e4342266cfce385c89c6906edeac4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-16 23:39:58 +0600

    Learning event

--- Milestone-5/Module-26/comment-box.js
+++ Milestone-5/Module-26/comment-box.js
@@ -1,7 +1,12 @@
+
+
+//step-1
 document.getElementById('post-btn').addEventListener('click', function(){
+  //step-2
    const commentBox = document.getElementById('comment-box');
    const boxText = commentBox.value;
    
+   //step-3
    const divContainer = document.getElementById('comment-container');
    const p = document.createElement('p');
    p.innerText = boxText;

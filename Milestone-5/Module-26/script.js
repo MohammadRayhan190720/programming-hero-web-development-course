9b3bb35fe08a7400385aa2cb8de06e69deed09e4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-16 11:27:28 +0600

    Learning click event

--- Milestone-5/Module-26/script.js
+++ Milestone-5/Module-26/script.js
@@ -1,4 +1,18 @@
+
+//option-2
 function makePurple(){
   document.body.style.backgroundColor = 'purple';
 }
 
+
+//option-3
+const pinkBtn = document.getElementById('pink-btn');
+pinkBtn.onclick = makePink;
+
+function makePink(){
+  document.body.style.backgroundColor = 'red';
+}
+
+//option-3 another
+
+const redBtn = document.getElementById('red-btn');

2b74f935133f5f9869cede92516848488b13cfb3
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-16 12:10:55 +0600

    Learning click event

--- Milestone-5/Module-26/script.js
+++ Milestone-5/Module-26/script.js
@@ -6,16 +6,37 @@ function makePurple(){
 
 
 //option-3
-const pinkBtn = document.getElementById('pink-btn');
-pinkBtn.onclick = makePink;
+const orangeBtn = document.getElementById('orange-btn');
+orangeBtn.onclick = makeOrange;
 
-function makePink(){
-  document.body.style.backgroundColor = 'red';
+function makeOrange(){
+  document.body.style.backgroundColor = 'orange';
 }
 
 //option-3 another
 
-const redBtn = document.getElementById('gray-btn');
-redBtn.onclick = function makeRed(){
+const grayBtn = document.getElementById('gray-btn');
+grayBtn.onclick = function makeGray(){
   document.body.style.backgroundColor = 'gray';
 }
+
+//option-4
+
+const makePink = document.getElementById('make-pink');
+makePink.addEventListener('click', makeColorPink);
+
+function makeColorPink(){
+  document.body.style.backgroundColor = 'Pink';
+}
+
+//option-4 another
+const makeGreen = document.getElementById('make-green');
+makeGreen.addEventListener('click', function makeColorGreen(){
+  document.body.style.backgroundColor = 'green';
+
+});
+
+//option-4 final and We mostly use this
+const makeBlue = document.getElementById('make-blue').addEventListener('click',function(){
+  document.body.style.backgroundColor = 'green';
+})

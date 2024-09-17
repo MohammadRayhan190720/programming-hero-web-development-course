b6d0a20bfb81ff153b90fbff41b57d57d106cec4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-17 08:05:54 +0600

    Learning event

---
+++ Milestone-5/Module-26/event-more.js
@@ -0,0 +1,16 @@
+document.getElementById('event-btn').addEventListener('click', function(){
+  console.log('clicked');
+})
+// document.getElementById('event-btn').addEventListener('mouseenter', function(){
+//   console.log('clicked');
+// })
+// document.getElementById('event-btn').addEventListener('mouseover', function(){
+//   console.log('clicked');
+// })
+// document.getElementById('event-btn').addEventListener('mousemove', function(){
+//   console.log('clicked');
+// })
+
+document.getElementById('input-text').addEventListener('focus',function(){
+  console.log("focus in input text");
+})

b38358e26e4ba5b42228335a314cba03931a1de2
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-19 11:47:06 +0600

    add event listener and preventdeafult

--- Milestone-5/Module-27/script.js
+++ Milestone-5/Module-27/script.js
@@ -1,13 +1,14 @@
 // find button
 const loginBtn = document.getElementById('login-btn');
 const inputNumber = document.getElementById('input-number');
+const inputPin = document.getElementById('input-pin');
 
 
 loginBtn.addEventListener('click',function(event){
   event.preventDefault();
 
-  console.log("login button clicked");
   console.log(inputNumber.value);
+  console.log(inputPin.value);
   
 
 });

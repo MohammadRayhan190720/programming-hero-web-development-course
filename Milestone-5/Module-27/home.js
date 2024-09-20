2704274022474d34df9aa738b3c12bc6b55fb537
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-20 19:42:01 +0600

    add money functionality added

--- Milestone-5/Module-27/home.js
+++ Milestone-5/Module-27/home.js
@@ -1,1 +1,22 @@
-console.log("js file added");
+
+
+document.getElementById("add-money-btn").addEventListener("click", function(event){
+  event.preventDefault();
+
+  const addMoney = document.getElementById("add-money").value;
+  const addMoneyNumber = parseFloat(addMoney);
+
+  const pin = document.getElementById("pin").value;
+  const pinNumber = parseFloat(pin);
+
+  // pin validate
+  if (pinNumber === 1234){
+    console.log("add money successfully");
+    const balance = document.getElementById("balance").innerText;
+    console.log(balance);
+  }else{
+    alert("add money failed");
+  }
+  
+
+})

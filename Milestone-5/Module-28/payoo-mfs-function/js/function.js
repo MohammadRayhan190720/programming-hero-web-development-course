e5f28778ba42499c2276a6c2c142c7759af07e3e
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-23 00:14:03 +0600

    function js

--- Milestone-5/Module-28/payoo-mfs-function/js/function.js
+++ Milestone-5/Module-28/payoo-mfs-function/js/function.js
@@ -1,4 +1,12 @@
-function getInputValueById(){
- const addMoney = document.getElementById("add-money").value ;
- return addMoney ;
+// function getInputValueById(){
+//  const addMoney = document.getElementById("add-money").value ;
+//  return addMoney ;
+// }
+
+
+// common function for input value
+
+function getInputValue (id){
+  const inputValue = document.getElementById(id).value ;
+  return inputValue;
 }
--- Milestone-5/Module-28/payoo-mfs-function/js/home.js
+++ Milestone-5/Module-28/payoo-mfs-function/js/home.js
@@ -1,7 +1,11 @@
 document.getElementById("add-money-btn").addEventListener("click", function(event){
   event.preventDefault();
 
-  const addInputValue = getInputValueById();
-  console.log(addInputValue);
+  // const addInputValue = getInputValueById();
+  // console.log(addInputValue);
+
+  const addMoneyValue = getInputValue("add-money");
+  const inputPinValue = getInputValue("pin");
+  console.log({addMoneyValue, inputPinValue});
   
 });

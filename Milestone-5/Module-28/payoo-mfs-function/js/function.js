f25db2c61e7f1efa45109d946cb4a8cc75ce3fba
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-23 09:40:01 +0600

    added toggle btn

--- Milestone-5/Module-28/payoo-mfs-function/js/function.js
+++ Milestone-5/Module-28/payoo-mfs-function/js/function.js
@@ -16,4 +16,14 @@ function getTextValue (id){
   const textValue = document.getElementById(id).innerText ;
   const textValueNumber = parseFloat(textValue);
   return textValueNumber ;
+};
+
+function showTogglebutton (id){
+  // hidden all form
+  document.getElementById("add-money-container").classList.add("hidden");
+  document.getElementById("cash-out-container").classList.add("hidden");
+  document.getElementById("Transaction-container").classList.add("hidden");
+  //show clicket button form
+  document.getElementById(id).classList.remove("hidden");
+
 }

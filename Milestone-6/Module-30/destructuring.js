d2c1cebaa4f6baafba7a6baef47181a5918542e3
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-29 09:55:41 +0600

    destructuring

--- Milestone-6/Module-30/destructuring.js
+++ Milestone-6/Module-30/destructuring.js
@@ -1,3 +1,6 @@
+
+//object destructuring
+
 const persons = {
   name : "Jonh",
   phone : "01723408269",
@@ -8,8 +11,24 @@ const persons = {
 const {phone,address} = persons;
 
 //change property name
-const  { name:identity, id} = persons;
+const  { name:identity, id} = persons;  //its called alias
 console.log(phone);
 console.log(address);
 // console.log(name);
 console.log(identity);
+
+//array destructuring
+const numbers = [20,30,40];
+const [num1,num2,num3] = numbers;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+const [x,y] = [45,50];
+
+//advanced destructuring
+function doubleIt(x,y){
+  return [x*2 , y*2];
+
+}
+const [number1,number2] = doubleIt(5,10);
+console.log(number1,number2);

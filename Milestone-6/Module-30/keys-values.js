79e358c6d5e4b175ac8d89e155ed5d134fb26552
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-29 10:37:47 +0600

    keys-values

--- Milestone-6/Module-30/keys-values.js
+++ Milestone-6/Module-30/keys-values.js
@@ -4,8 +4,27 @@ const glass = {
   color : "golden",
   isClean : true,
 }
+
+//keys
 const keys = Object.keys(glass);
 console.log(keys);
 
+//values
 const values = Object.values(glass);
 console.log(values);
+
+//keys and values =>return array of arrays
+const keysAndValues = Object.entries(glass);
+
+//delete properties
+// delete glass.isClean;
+console.log(glass)
+//destructuring delete
+const {isClean, ...shortGlass} = glass ;
+console.log(shortGlass);
+
+//freeze => not allowed to change anything of an object
+// Object.freeze(glass);
+
+//seal => allowed to change exesting properties and values but not allowed to add properties
+//Object.seal(glass);

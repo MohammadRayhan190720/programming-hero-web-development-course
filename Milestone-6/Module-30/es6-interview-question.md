60a2be7d714f4676c43bfe3b8ad2e947726b8082
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-29 11:31:27 +0600

    practice task added

---
+++ Milestone-6/Module-30/es6-interview-question.md
@@ -0,0 +1,11 @@
+### ES6 Common interview question
+1.what is ES6?Have you ever used anything form ES6?
+2.Explain the difference between var,let and Const?
+3.What is the arrow function and How to creat it?
+4.Give an Example for the arrow function in ES6? list down it advantaged?
+5.Discuss spread operator in ES6 with an Example?
+6.What do you understand about default parameters?
+7.What are the templeted literals in ES6?
+8.Tell us the difference between regular function and Arrow function?
+9.Tell us the difference between seal and freeze?
+10.Tell us the difference between for of and For in?
---
+++ Milestone-6/Module-30/practice-1.js
@@ -0,0 +1,36 @@
+/***
+ * Write a arrow function that will take 3 parameters ,will multiply the parametes and will return the result
+ */
+
+/**
+ * Write a following sentence in Three lines and Print the result
+ * I am a fronted web developer. I always love code.I want to be a successful web developer.
+ * 
+ */
+
+/**
+ * write a arrow function that will take 2 parameters ,one parameter come form you and other is default.add these two parameters and retiurn the result
+ */
+
+/**
+ * write a arrow function whwre it will do the following
+ * 1.it will take an array where the array element will name of your friends
+ * 2.Check if the length of each element is even ,push elements with even length to a new array and return the result
+ */
+
+/**
+ * write a arrow function whwre it will do the following
+ * 1.squire each array element
+ * 2.calculate the sum of squire elements
+ * 3.return the average of the sum of the squired element
+ */
+
+/**
+ * write a arrow function where it will do the following
+ * 1.it will take two arrays input
+ * 2.combine two arrays and assain them in a new array
+ * 3.find the maximum number from array and return the result
+ */
+
+
+

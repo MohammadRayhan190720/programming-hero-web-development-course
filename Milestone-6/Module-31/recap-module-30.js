0d25ed010d1a7c38425ec8ecc1896f900cc05fe0
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-30 08:39:46 +0600

    module-31

---
+++ Milestone-6/Module-31/recap-module-30.js
@@ -0,0 +1,11 @@
+/**
+ * what I Learn form previous module
+ * 1.Var,let,const
+ * 2.default parameter
+ * 3.Template letarate
+ * 4.Arrow Function
+ * 5.Destrucring and Spread operator
+ * 6.Object.keys Object.values , Object.entries , object.freezze , object.seal 
+ * 7.for of used in array
+ * 8.for in loop use in object
+ */

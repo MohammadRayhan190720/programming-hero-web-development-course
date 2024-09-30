afb807782ef795fbfc85ddab0bdbd806cd5be896
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-30 11:56:56 +0600

    foreach,filter,reduce,map,find recap

--- Milestone-6/Module-31/array-object.js
+++ Milestone-6/Module-31/array-object.js
@@ -1,8 +1,9 @@
 const products = [
-  { id : 1, name : "HP" , price : 800000},
-  { id : 2, name : "Dell" , price : 600000},
-  { id : 3, name : "Apple" , price : 1800000},
-  { id : 4, name : "Lenovo" , price : 450000}
+  { id : 1, name : "HP" , price : 80000},
+  { id : 2, name : "Dell" , price : 60000},
+  { id : 3, name : "Apple" , price : 180000},
+  { id : 4, name : "Lenovo" , price : 45000},
+  { id : 5, name : "XXX" , price : 47000}
 ];
 
 //map
@@ -14,3 +15,15 @@ console.log(prices);
 
 //forEach
 products.forEach(product => console.log(product.id));
+
+//filter 
+const expences = products.filter(product => product.price > 60000);
+console.log(expences);
+
+//find
+const affortable = products.find(product => product.price < 50000 );
+console.log(affortable);
+
+//reduce
+const totalPrice = products.reduce((previous,current) => previous + current.price, 0);
+console.log(totalPrice);

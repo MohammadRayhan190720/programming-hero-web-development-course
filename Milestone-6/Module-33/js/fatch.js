4b8cfb9972581ca054d7b98835b7877f8fa2da10
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-05 21:34:11 +0600

    fetch api

--- Milestone-6/Module-33/js/fatch.js
+++ Milestone-6/Module-33/js/fatch.js
@@ -1,4 +1,20 @@
 
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// way-2
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+fetch(url)
+.then((response) => response.json())
+.then((json) => console.log(json));
+
+
+// way-3
+function loadData(){
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+}

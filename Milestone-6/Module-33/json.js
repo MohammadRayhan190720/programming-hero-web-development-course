945fef0a129881909c66de148a850c1b15facda0
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-05 19:46:52 +0600

    Json

--- Milestone-6/Module-33/json.js
+++ Milestone-6/Module-33/json.js
@@ -1,3 +1,5 @@
 const people = {id : 1, name : "rayhan", age :24, classification : "Honours"};
 const stringfied = JSON.stringify(people);
 console.log(stringfied);
+const parsefy = JSON.parse(stringfied);
+console.log(parsefy);

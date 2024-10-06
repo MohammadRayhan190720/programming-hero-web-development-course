3b6268748b2c7962671af9ca1ac07209273ac3d6
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-06 17:36:21 +0600

    async await

---
+++ Milestone-6/Module-33/interview-question.md
@@ -0,0 +1,13 @@
+### common interview question
+- What is an API?
+- What are the HTTP methods supported by REST?
+- Can you use get Request instead of PUT create a resources?
+- What is the different between PUT and Post?
+- What is JSON?
+- What are the CRUD Operations?
+- what is the file extension of JSON ?
+- What are the data types supported by JSON ?
+- What is the role of json.stringify() ?
+- Show how to parse a JSON ?
+- Tell us the different between get and post ?
+
--- Milestone-6/Module-33/js/comments.js
+++ Milestone-6/Module-33/js/comments.js
@@ -1,6 +1,19 @@
 function loadComment(){
   fetch("https://jsonplaceholder.typicode.com/comments")
-    .then((res) => res.js())
+    .then((res) => res.json())
     .then((data) => console.log(data))
     .catch((err) => console.error('error happent',err));
+}
+
+//use async-await
+const loadComment2 = async() =>{
+  try{
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments")
+    const data = await res.json();
+    console.log(data);
+  }
+  catch(err){
+    console.error('error loading data',err);
+  }
+
 }

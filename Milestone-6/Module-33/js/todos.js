15fc492841cf6936d2b02afb74db9af4981d82f0
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-06 09:05:40 +0600

    post data

--- Milestone-6/Module-33/js/todos.js
+++ Milestone-6/Module-33/js/todos.js
@@ -6,7 +6,17 @@ function displayTodos(todos){
   const todosContainer = document.getElementById('todos-container');
 
   for (const todo of todos) {
-    console.log(todo);
+    console.log(todo)
+    
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo-style');
+    todoDiv.innerHTML = `
+    <h3>User id : ${todo.id}</h3>
+    <P>IsCompleted : ${todo.completed}</P>
+    <P>Title : ${todo.title}</P>
+
+    `
+    todosContainer.appendChild(todoDiv);
     
   }
 }

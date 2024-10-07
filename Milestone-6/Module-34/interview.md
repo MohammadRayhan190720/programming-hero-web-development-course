1b83989df811daf87ce56fe0861308829dbd1b7a
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-07 10:14:06 +0600

    setInterval and setTimeoUt

---
+++ Milestone-6/Module-34/interview.md
@@ -0,0 +1,7 @@
+#### Important Interview Question
+- Tell me something about Js engine V8 internal mechanism
+- What is event loop in Javascript ?
+- If javascript is a single threaded how does it handle asynchronous call ?
+- Different between setTimeout and setInterval ?
+- can you explain between async await and promise ?
+- Can you explain what does .then method does ?
---
+++ Milestone-6/Module-34/js/practice-task.js
@@ -0,0 +1,48 @@
+/**
+ * Practic task-1 
+ * a. Write a function that displays a message after 5s?
+ * b.Write a function call delayedGreeting() that takes two parameters,name and delay time and logs a greeting message after the given delay time?
+ * 
+ */
+
+// function displayMessage(){
+//   setInterval(() =>{
+//     console.log('Hellow')
+//   },5000)
+// }
+// displayMessage();
+
+// function displayGreeting(name, delayTime){
+//   setInterval(() =>{
+//     console.log(`Hellow ${name}`)
+//   },2000)
+// }
+// displayGreeting('Alice', 2000);
+
+/**
+ * The function tellJoke() below logs a funny message every 2 seconds after 10 seconds it stops 
+ */
+
+function tellJoke(){
+    const jokes = [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Parallel lines have so much in common. It's a shame they'll never meet.",
+        "I told my wife she should embrace her mistakes. She gave me a hug.",
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Parallel lines have so much in common. It's a shame they'll never meet.",
+        "I told my wife she should embrace her mistakes. She gave me a hug."
+    ];
+    let count = 0;
+ const jokeInterval = setInterval(() =>{
+   if ( count < jokes.length){
+    console.log(jokes[count])
+    count++;
+   }
+ 
+ },2000);
+
+ setTimeout(() => clearInterval(jokeInterval),10000);
+
+ }
+
+ tellJoke();

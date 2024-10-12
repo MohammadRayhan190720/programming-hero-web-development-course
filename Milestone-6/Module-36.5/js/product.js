560d9f5fa86dc932f160aa30ed250c9ddba6a3e7
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-12 19:18:37 +0600

    add product

--- Milestone-6/Module-36.5/js/product.js
+++ Milestone-6/Module-36.5/js/product.js
@@ -1,5 +1,31 @@
 const addProduct = () =>{
   const product = document.getElementById("product-name").value ;
   const quentity = document.getElementById("product-quentity").value ;
-  console.log(product,quentity)
+  displayProduct(product,quentity)
+  saveLocalstroage(product,localStorage)
+}
+
+const displayProduct = (product,quentity) =>{
+  const productContainer = document.getElementById("product-container");
+  const li = document.createElement("li");
+  li.innerText = `${product} ${quentity}`;
+
+  productContainer.appendChild(li);
+
+}
+
+const getShopingCart = () =>{
+  let cart = {};
+  const productCart = localStorage.getItem("cart");
+
+  if (productCart){
+    cart = JSON.stringify(productCart);
+  }
+  return cart;
+}
+
+
+const saveLocalstroage = (product,quentity) =>{
+  const stroage = localStorage.setItem(`${product} , ${quentity}`);
+
 }

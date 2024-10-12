7af3ef1be0837796aa5cb7eb82e6eebf95daefed
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-10-12 18:33:14 +0600

    add product

---
+++ Milestone-6/Module-36.5/js/product.js
@@ -0,0 +1,5 @@
+const addProduct = () =>{
+  const product = document.getElementById("product-name").value ;
+  const quentity = document.getElementById("product-quentity").value ;
+  console.log(product,quentity)
+}
